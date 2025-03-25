dff8067d333d1c84f038b56f115680fb20150fb5
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-03-25 14:50:35 +0300

    Готово 1.0.0

--- script.js
+++ script.js
@@ -1,84 +1,71 @@
-'use strict';
-
-const japaneseRestaurant = {
-  name: 'Banzai',
-  location: '108 Markham Woods Rd, Longwood, USA',
-  categories: ['Japanese', 'Sushi', 'Vegetarian', 'Organic'],
-  appetizers: ['Seaweed salad', 'Tempura shrimp', 'Edamame', 'Sushi rice'],
-  mainMenu: ['Sushi', 'Ramen', 'Tempura'],
-
-  workingHours: {
-    wed: {
-      open: 10,
-      close: 23,},
-      fri: {
-        open: 10,
-        close: 23
-      },
-      sun: {
-        open: 12,
-        close: 23
-      }
+const game = {
+  team1: 'REAL MADRID',
+  team2: 'BARCELONA',
+  players: [
+    [
+      'Courtois',
+      'Vazquez',
+      'Militao',
+      'Nacho',
+      'Mendy',
+      'Casemiro',
+      'Valverde',
+      'Modrich',
+      'Kroos',
+      'Vinicius',
+      'Benzema',
+    ],
+    [
+      'Stegen',
+      'Mingueza',
+      'Araujo',
+      'Lenglet',
+      'Dest',
+      'Busquets',
+      'Jong',
+      'Alba',
+      'Messi',
+      'Pedri',
+      'Dembele',
+    ],
+  ],
+  score: '2:1',
+  scored: ['Kroos', 'Benzema', 'Mingueza'],
+  date: 'Apr 10th, 2021',
+  odds: {
+    team1: 1.48,
+    draw: 2.53,
+    team2: 4.25,
   },
-
-  orderFood: function (starterIndex, mainIndex) {
-    return [this.appetizers[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  foodDelivery: function ({mainMenuIndex, appetizersIndex, time, deliveryTime}) {
-    console.log(`Order received! ${this.mainMenu[mainMenuIndex]} and ${this.appetizers[appetizersIndex]} will arrive in ${time} minutes, at ${deliveryTime}`);
-  }
 };
 
-japaneseRestaurant.foodDelivery({
-  mainMenuIndex: 1,
-  appetizersIndex: 1,
-  time: 45,
-  deliveryTime: '2022-03-18 14:15'
-});
+const [players1, players2] = game.players;
+const [goalKeeper1, ...fieldPlayers1] = players1;
+const allPlayers = [...players1, ...players2];
+
+const players1Total = [
+  ...players1,
+  'Marcelo',
+  'Isco',
+  'Asensio',
+  'Diaz',
+  'Odriozola',
+];
+
+const {
+  odds: { team1, draw, team2 },
+} = game;
+
+const printGoals = function (...gamePlayers) {
+  console.log(gamePlayers);
+  console.log(`${gamePlayers.length} goals were scored in the game`);
+};
+
+printGoals('Marcelo', 'Isco', 'Asensio', 'Diaz', 'Odriozola');
+printGoals(...game.scored);
 
 
-// Диструктуризация объекта
-
-const {workingHours: hours, name: restName, categories} = japaneseRestaurant;
-console.log(hours, restName, categories);
 
 
-const {sun} = hours;
-console.log(sun);
-
-// Диструктуризация массива
-// const arr = [3, 5, 7];
-
-// const x1 = arr[0];
-// const x2 = arr[1];
-// const x3 = arr[2];
-
-// const [y1, y2, y3] = arr;
-// console.log(y1, y2, y3);
-
-// console.log(x1, x2, x3);
-
-// console.log(arr);
-
-// let [main, , secondary] = japaneseRestaurant.categories;
-// console.log(main, secondary);
-
-// const temp = main;
-// main = secondary;
-// secondary = temp;
-// console.log(main, secondary);
-
-// [main, secondary] = [secondary, main];
-// console.log(main, secondary);
-
-// const [appetizer, mainFood] = japaneseRestaurant.orderFood(2, 1);
-
-// console.log(appetizer, mainFood);
-
-// const nestedArr = [1, 2, [7, 9]];
-
-// const [f, , d] = nestedArr;
-
-// const [f, , [d, e]] = nestedArr;
-// console.log(f, d, e);
+ team1 < team2 && console.log(`${game.team1} is more likely to win`);
+ team1 > team2 && console.log(`${game.team2} is more likely to win`);
