8194993ac05cbe734e3bf21bdf231c465e20324b
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-03-26 17:08:04 +0300

    130  готов

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const bookings = [];
+
+const makeBooking = function (flightNum, passengersNum = 1, price = 100 * passengersNum) {
+  const booking = {
+    flightNum,
+    passengersNum,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+
+makeBooking('LH123', 2, 80);
+makeBooking('LH123', 2);
+
+
