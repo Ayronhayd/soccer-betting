0b75bfc5f150e22258c8848ed919b294f9a55807
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-04-06 15:22:32 +0300

    0.0.0

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-const survey = {
-  question: 'Какой язык программирования вы хотели бы выучить?',
-  options: ['0: JavaScript', '1: Python', '2: Ruby', '3: Java', '4: C#'],
-  answers: new Array(5).fill(0),
+// (IIFE) Immediately Invoked Function Expression
 
-  logNewAnswer() {
-    // display a prompt
-    const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Напишите номер ответа)`));
-    console.log(`Ваш ответ: ${answer}`);
+const runOneTime = function () {
+  console.log("вы больше никогда не увидите вызов этой функции");
+}
 
-    // Update answers
-    if (typeof answer === 'number' && answer >= 0 && answer < this.answers.length) {
-      this.answers[answer]++;
-      console.log(this.answers);
-    }
+// runOneTime();
+// runOneTime();
 
-    },
-  
-    printResults(type = 'array') {
-      if (type === 'array') {
-        console.log(this.answers);
-      } else if (type === 'string') {
-        console.log(`Poll results are ${this.answers.join(', ')}`);
-      }
-  }
-};
-
-document.querySelector('#survey').addEventListener('click', survey.logNewAnswer.bind(survey));
-
-//  survey.logNewAnswer();
+(function () {
+  console.log("вы больше никогда не увидите вызов этой функции");
+})();
