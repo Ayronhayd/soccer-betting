f2aa3259813ad8bb57e70e7ecbaeae724827a38d
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-04-06 15:14:26 +0300

    0.0.0

--- script.js
+++ script.js
@@ -7,17 +7,24 @@ const survey = {
     // display a prompt
     const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Напишите номер ответа)`));
     console.log(`Ваш ответ: ${answer}`);
-    
 
     // Update answers
-
     if (typeof answer === 'number' && answer >= 0 && answer < this.answers.length) {
       this.answers[answer]++;
       console.log(this.answers);
-      
     }
-  },
- };
 
- survey.logNewAnswer();
- 
+    },
+  
+    printResults(type = 'array') {
+      if (type === 'array') {
+        console.log(this.answers);
+      } else if (type === 'string') {
+        console.log(`Poll results are ${this.answers.join(', ')}`);
+      }
+  }
+};
+
+document.querySelector('#survey').addEventListener('click', survey.logNewAnswer.bind(survey));
+
+//  survey.logNewAnswer();
