565468fa0fc5c1cc2bc2d08c64cda074bc5246f5
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-04-06 15:30:26 +0300

    Добавление стрелочной функции и обновление вывода в IIFE

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // (IIFE) Immediately Invoked Function Expression
 
-const runOneTime = function () {
-  console.log("вы больше никогда не увидите вызов этой функции");
-}
+// const runOneTime = function () {
+//   console.log("вы больше никогда не увидите вызов этой функции 0");
+// }
 
 // runOneTime();
 // runOneTime();
 
 (function () {
-  console.log("вы больше никогда не увидите вызов этой функции");
+  console.log("вы больше никогда не увидите вызов этой функции 1");
 })();
+
+( () => {
+  console.log("вы больше никогда не увидите вызов этой стрелочной функции 2");
+}
+)();
